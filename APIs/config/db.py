45ac5eb62d8d7890bb95122c4e7921dc057670e3
joseorozco84 <joseorozco84@gmail.com>
2023-09-01 12:29:12 -0300

    main

--- APIs/config/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-
-user = "admin"
-password = "Genius2008"
-
-engine = create_engine(f"mysql+pymysql://{user}:{password}@database-farmacia.cccupuslvvw9.us-east-2.rds.amazonaws.com/farmacia")
-
-meta_data = MetaData()
