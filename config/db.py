6aa805c455f1fd6b3d408fcae03c1d7c2856acb6
joseorozco84 <joseorozco84@gmail.com>
2023-09-03 19:47:44 -0300

    hostinger

--- config/db.py
+++ config/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, MetaData
 
-user = "visitante"
-password = "Visitante123"
+user = "u853169718_joseorozco"
+password = "yZHW]>&Rz>8"
 
-engine = create_engine(f"mysql+pymysql://{user}:{password}@database-farmacia.cccupuslvvw9.us-east-2.rds.amazonaws.com/farmacia")
+engine = create_engine(f"mysql+pymysql://{user}:{password}@srv995.hstgr.io/u853169718_farmacia")
 
 meta_data = MetaData()
