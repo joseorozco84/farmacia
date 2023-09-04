cdcfa7f0ddaf25bdbf046ba0852479b8130ed7eb
joseorozco84 <joseorozco84@gmail.com>
2023-09-04 00:07:26 -0300

    asyncio

--- router/router.py
+++ router/router.py
@@ -21,6 +21,7 @@ async def root():
 @user.get("/api/user", response_model=List[UserSchema], tags=["usuario"])
 async def get_users():
     with engine.begin() as conn:
+        await asyncio.sleep(1)
         result = conn.execute(users.select()).fetchall()
         return result
 
@@ -29,15 +30,17 @@ async def get_users():
 async def create_user(data_user: UserSchema):
     with engine.begin() as conn:
         new_user = data_user.model_dump()
-        await conn.execute(users.insert().values(new_user))
+        await asyncio.sleep(1)
+        conn.execute(users.insert().values(new_user))
         return Response(status_code=HTTP_201_CREATED)
 
 # actualizar usuario
 @user.patch("/api/user/{id}", response_model=UpdateUserSchema, tags=["usuario"])
 async def update_user(data_update: UpdateUserSchema, id: int):
     with engine.begin() as conn:
-        await conn.execute(users.update().values(data_update.model_dump(exclude_unset=True)).where(users.c.id == id))
-        result = await conn.execute(users.select().where(users.c.id == id)).first()
+        await asyncio.sleep(1)
+        conn.execute(users.update().values(data_update.model_dump(exclude_unset=True)).where(users.c.id == id))
+        result = conn.execute(users.select().where(users.c.id == id)).first()
         print(result)
         return Response(status_code=HTTP_200_OK)
     
@@ -45,13 +48,15 @@ async def update_user(data_update: UpdateUserSchema, id: int):
 @user.delete("/api/user/{id}", response_model=UserSchema, tags=["usuario"])
 async def delete_user(id: int):
     with engine.begin() as conn:
-        await conn.execute(users.delete().where(users.c.id == id))
+        await asyncio.sleep(1)
+        conn.execute(users.delete().where(users.c.id == id))
         return Response(status_code=HTTP_204_NO_CONTENT)
 
 # ver productos
 @producto.get("/api/producto", response_model=List[ProductoSchema], tags=["producto"])
 async def get_producto():
     with engine.begin() as conn:
+        await asyncio.sleep(1)
         result = conn.execute(productos.select()).fetchall()
         return result
     
@@ -59,7 +64,8 @@ async def get_producto():
 @producto.get("/api/producto/{id_producto}", response_model=ProductoSchema, tags=["producto"])
 async def get_producto_by_id(id_producto: int):
     with engine.begin() as conn:
-        result = await conn.execute(productos.select().where(productos.c.id_producto == id_producto)).first()
+        await asyncio.sleep(1)
+        result = conn.execute(productos.select().where(productos.c.id_producto == id_producto)).first()
         return result
     
 # crear producto
@@ -67,19 +73,22 @@ async def get_producto_by_id(id_producto: int):
 async def create_producto(data_producto: ProductoSchema):
     with engine.begin() as conn:
         new_prod = data_producto.model_dump()
-        await conn.execute(productos.insert().values(new_prod))
+        await asyncio.sleep(1)
+        conn.execute(productos.insert().values(new_prod))
     return Response(status_code=HTTP_201_CREATED)
 
 # actualizar producto
 @producto.patch("/api/producto/{id_producto}", response_model=UpdateProductoSchema,tags=["producto"])
 async def update_producto_by_id(data_producto: UpdateProductoSchema, id_producto: int):
     with engine.begin() as conn:
-        await conn.execute(productos.update().values(data_producto.model_dump(exclude_unset=True)).where(productos.c.id_producto == id_producto))
+        await asyncio.sleep(1)
+        conn.execute(productos.update().values(data_producto.model_dump(exclude_unset=True)).where(productos.c.id_producto == id_producto))
         return Response(status_code=HTTP_200_OK)
 
 # eliminar producto
 @producto.delete("/api/producto/{id_producto}", response_model=ProductoSchema, tags=["producto"])
 async def delete_producto_by_id(id_producto: int):
     with engine.begin() as conn:
-        await conn.execute(productos.delete().where(productos.c.id_producto == id_producto))
+        await asyncio.sleep(1)
+        conn.execute(productos.delete().where(productos.c.id_producto == id_producto))
         return Response(status_code=HTTP_204_NO_CONTENT)
